41180504a0382dab767f7422c8b60ab08248da8a
young <317669016@qq.com>
2016-01-23 10:34:32 +0800

    add quartz
    接入quartz框架

---
+++ src/main/java/org/andy/shop/controller/TimedTask.java
@@ -0,0 +1,16 @@
+package org.andy.shop.controller;
+
+/**
+ * Created by CIFPAY on 2016/1/23.
+ */
+public class TimedTask {
+
+    /**
+     * 业务逻辑处理
+     */
+    public void execute() {
+        // 执行业务逻辑
+        // ........
+        System.out.println(System.currentTimeMillis()+"定时任务.......");
+    }
+}
